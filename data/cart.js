ad1148b8e0607ecbd37da039f651c496c22a164d
AmareshSahoo <amareshsahoo950@gmail.com>
2024-01-07 14:42:11 +0530

    saved cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'))
+||
+[{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2,
 },
@@ -6,6 +8,23 @@ export let cart = [{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }]
+
+//If we don't have any value in the cart in that case the cart will contain null .
+// or we can also write is as -
+//  if(!cart){
+//   cart = [{
+//     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+//     quantity: 2,
+//   },
+//   {
+//     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+//     quantity:1
+//   }]
+//  }
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId)
 {
@@ -50,10 +69,12 @@ export function addToCart(productId)
     //           });
     //    }
 
-  }
+      saveToStorage();
+  
+ }
 
 export function removeFromCart(productId)
-    {
+  {
       const newCart = [];
 
       cart.forEach((cartItem)=>{
@@ -63,5 +84,7 @@ export function removeFromCart(productId)
       });
 
       cart = newCart;
+
+      saveToStorage();
     }
   
